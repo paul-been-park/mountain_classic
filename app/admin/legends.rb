cd4b0933ccdbe2b06671e1778ea2b2df22d4f2cb
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-03-15 02:52:22 +0000

    Whitelist columns in Legend for ActiveAdmin

--- app/admin/legends.rb
+++ app/admin/legends.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Legend do
 
+ permit_params :name, :photo, :bio, :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
