129d83af292c7ab92d6ea2852334ede66b1854a4
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-03-15 02:52:23 +0000

    Whitelist columns in FirstAscent for ActiveAdmin

--- app/admin/first_ascents.rb
+++ app/admin/first_ascents.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FirstAscent do
 
+ permit_params :legend_id, :climb_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
