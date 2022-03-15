ded0ec0989555ba1da4ecbbaea4bea714311b18d
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-03-15 02:52:25 +0000

    Whitelist columns in Interaction for ActiveAdmin

--- app/admin/interactions.rb
+++ app/admin/interactions.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Interaction do
 
+ permit_params :tick, :summited, :user_id, :climb_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
