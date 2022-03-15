d7de1a7655a78f18435488bccfa6e50a6c475cf8
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-03-15 02:52:14 +0000

    Whitelist columns in Mountain for ActiveAdmin

--- app/admin/mountains.rb
+++ app/admin/mountains.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Mountain do
 
+ permit_params :region_id, :mountain_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
